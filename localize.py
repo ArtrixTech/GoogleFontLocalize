2c38b43e9a2dac75c94de0fabbe4c17201e46837
ArtrixTech <Artrix@artrix.tech>
2018-12-23 00:31:55 +0800

    [Download] Added module

--- localize.py
+++ localize.py
@@ -1,4 +1,6 @@
 import requests
+import os
+
 from cut_string import cut_string
 
 
@@ -20,20 +22,37 @@ url_base = "https://fonts.googleapis.com/css?family="
 font_family = input("Please input the font family you wanna localize:\n")
 
 resp = requests.get(url_base + font_family).text
+
 if err_handle(resp):
+
     all_fonts = []
-    dest_dir = input("Please input the local resource destination directory:\n")
+    dest_dir = input("Please input the local resource destination directory (eg: docs\\):\n")
 
     blocks = resp.split(";")
     index = 0
+
     for item in blocks:
         if "url" in item:
             download_item = cut_string(item, "url(", ")")
             expand_name = download_item.split(".")[-1]
+            font_obj = Font(font_family, font_family + "_" + str(index) + "." + expand_name, download_item)
+            all_fonts.append(font_obj)
 
-            all_fonts.append(Font(font_family, font_family + "_" + str(index) +"."+ expand_name, download_item))
+            # Replace the CSS content here
+            resp = resp.replace(font_obj.download_address, dest_dir + font_obj.file_name)
 
     for font in all_fonts:
         font_file_name = font.file_name
-        font_download_address=font.download_address
-        
+        font_download_address = font.download_address
+        font_content = requests.get(font_download_address).content
+
+        if not os.path.exists("saved"):
+            os.mkdir("saved")
+
+        # TODO: platform check
+        save_loc = "saved\\" + font_file_name
+        with open(save_loc, "wb") as file:
+            file.write(font_content)
+
+    with open("saved\\" + font_family + ".css", "w") as file:
+        file.write(resp)
